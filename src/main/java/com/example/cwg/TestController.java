83be2593ec3db2cf0a7767bcd7018cddfddb5808
1078427121 <1078427121@qq.com>
2020-05-26 08:51:37 +0800

    first

---
+++ src/main/java/com/example/cwg/TestController.java
@@ -0,0 +1,21 @@
+package com.example.cwg;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/get")
+    public List<Map<String,Object>> get(){
+        return jdbcTemplate.queryForList("select * from `user`;");
+    }
+}
